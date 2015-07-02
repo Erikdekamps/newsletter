590a3fb2a8168798bafdca542c9b02b21cd140fd
Erik de Kamps <Erik@luciuswebsystems.nl>
2015-07-02 16:28:22 +0200

    Update alignment.

--- profiles/automated_newsletters/themes/newsletter/templates/paragraphs/paragraphs-item--image-middle.tpl.php
+++ profiles/automated_newsletters/themes/newsletter/templates/paragraphs/paragraphs-item--image-middle.tpl.php
@@ -1,29 +1,15 @@
 <tr>
   <td colspan="3">
     <table width="600" align="center" cellpadding="0" cellspacing="0">
-      <tbody><tr>
+      <tbody>
+      <tr>
         <td width="14" height="16"></td>
         <td width="16" bgcolor="#ffffff"></td>
         <td bgcolor="#ffffff"></td>
         <td width="16" bgcolor="#ffffff"></td>
         <td width="14"></td>
       </tr>
-      <tr>
-        <td width="14" height="16"></td>
-        <td width="16" height="16" bgcolor="#ffffff"></td>
-        <td bgcolor="#ffffff">
-          <?php print isset($image) ? $image : ''; ?>
-        </td>
-        <td width="16" height="16" bgcolor="#ffffff"></td>
-        <td width="14" height="16"></td>
-      </tr>
-      <tr>
-        <td width="14" height="10"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td bgcolor="#ffffff"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td width="14"></td>
-      </tr>
+      <?php if (!empty($title)): ?>
       <tr>
         <td width="14" height="16"></td>
         <td width="16" height="16" bgcolor="#ffffff"></td>
@@ -35,12 +21,22 @@
         <td width="16" height="16" bgcolor="#ffffff"></td>
         <td width="14" height="16"></td>
       </tr>
+        <tr>
+          <td width="14" height="16"></td>
+          <td width="16" bgcolor="#ffffff"></td>
+          <td bgcolor="#ffffff"></td>
+          <td width="16" bgcolor="#ffffff"></td>
+          <td width="14"></td>
+        </tr>
+      <?php endif; ?>
       <tr>
-        <td width="14" height="10"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td bgcolor="#ffffff"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td width="14"></td>
+        <td width="14" height="16"></td>
+        <td width="16" height="16" bgcolor="#ffffff"></td>
+        <td bgcolor="#ffffff">
+          <?php print isset($image) ? $image : ''; ?>
+        </td>
+        <td width="16" height="16" bgcolor="#ffffff"></td>
+        <td width="14" height="16"></td>
       </tr>
       <tr>
         <td width="14"></td>
@@ -48,14 +44,6 @@
         <td bgcolor="#ffffff" style="color: #000001; font-size: 12px; line-height: 23px; font-family: Arial, Helvetica, sans-serif;">
           <?php print isset($text) ? $text : ''; ?>
         </td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td width="14"></td>
-      </tr>
-
-      <tr>
-        <td width="14" height="16"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td bgcolor="#ffffff"></td>
         <td width="16" bgcolor="#ffffff"></td>
         <td width="14"></td>
       </tr>
--- profiles/automated_newsletters/themes/newsletter/templates/paragraphs/paragraphs-item--regular.tpl.php
+++ profiles/automated_newsletters/themes/newsletter/templates/paragraphs/paragraphs-item--regular.tpl.php
@@ -10,6 +10,7 @@
           <td width="16" bgcolor="#ffffff"></td>
           <td width="14"></td>
         </tr>
+        <?php endif; ?>
         <tr>
           <td width="14" height="16"></td>
           <td width="16" height="16" bgcolor="#ffffff"></td>
@@ -28,20 +29,12 @@
           <td width="16" bgcolor="#ffffff"></td>
           <td width="14"></td>
         </tr>
-      <?php endif; ?>
       <tr>
         <td width="14"></td>
         <td width="16" bgcolor="#ffffff"></td>
         <td bgcolor="#ffffff" style="color: #000001; font-size: 12px; line-height: 23px; font-family: Arial, Helvetica, sans-serif;">
           <?php print isset($text) ? $text : ''; ?>
         </td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td width="14"></td>
-      </tr>
-      <tr>
-        <td width="14" height="16"></td>
-        <td width="16" bgcolor="#ffffff"></td>
-        <td bgcolor="#ffffff"></td>
         <td width="16" bgcolor="#ffffff"></td>
         <td width="14"></td>
       </tr>
